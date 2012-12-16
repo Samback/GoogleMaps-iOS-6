f500b9a18ab6a874d8813cc837cf5966a629f7a5
Max Tymchii <samback@ukr.net>
2012-12-16 15:11:20 +0200

    Implement Google maps framework

--- iOS/GoogleMapsBack/GoogleMapsBack/Helpers/Helper.h
+++ iOS/GoogleMapsBack/GoogleMapsBack/Helpers/Helper.h
@@ -1,14 +1,3 @@
-//
-//  Helper.h
-//  GoogleMapsBack
-//
-//  Created by Max on 16.12.12.
-//  Copyright (c) 2012 Max Tymchii. All rights reserved.
-//
-
-#ifndef GoogleMapsBack_Helper_h
-#define GoogleMapsBack_Helper_h
-
-
-
-#endif
+#define GOOGLE_CLIENT_ID @"568694354087.apps.googleusercontent.com"
+#define GOOGLE_SECRET_ID @"K7QX0Hwk7z7FPjyVJLBOTlLe"
+#define ApplicationDelegate                 ((GMAppDelegate *)[[UIApplication sharedApplication] delegate])
